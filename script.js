ecbb4732d16db475c8ba3d305db93a608460adc5
Lkoenig2121 <49110001+Lkoenig2121@users.noreply.github.com>
2022-07-15 14:33:53 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function displayMeal(attendance){
+    if(name=='lstbox') {
+      document.getElementById('div1').style.display="block";
+    } else {
+      document.getElementById('div1').style.display="none";
+    }    
+  }      
